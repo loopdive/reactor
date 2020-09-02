a5c3678959e3ae504f6886a3aeb0e25808d68f01
Thomas Tränkler <github.com@thomas.traenkler.com>
2020-09-02 14:20:25 +0200

    updated title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: FC = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Teamups</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
