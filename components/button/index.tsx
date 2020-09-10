61880092de4d8924dfbef94a6df8513b7d9e47ff
theinfoarchitect <theinfoarchitect@gmail.com>
2020-09-10 15:32:18 +0200

    Hover effect for button

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -11,9 +11,21 @@ export const Component: FC<Props> = ({ children, onChange }) => {
 };
 
 const Button = styled.span`
+  display: inline-block;
   padding: 15px 25px;
   border-radius: 25px;
   background-color: #429cd6;
+  color: white;
+  cursor: pointer;
+
+  box-shadow: 0px 10px 20px -5px rgba(0, 0, 0, 0.3);
+  transition: box-shadow 0.5s, transform 0.5s;
+  will-change: transform;
+
+  &:hover {
+    box-shadow: 0px 15px 30px -10px rgba(0, 0, 0, 0.3);
+    transform: translateY(-3px);
+  }
 `;
 
 export default Component;
