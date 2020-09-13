d5a705ab17577769779f029f680de581befed1bd
Thomas Tränkler <github.com@thomas.traenkler.com>
2020-09-13 14:37:32 +0200

    added story for dropdown

---
+++ components/DropdownMenu/story.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from "react";
+import DropdownMenu from ".";
+import { OnClick } from "../types";
+import ToggleButton from "../themes/default/ToggleButton";
+import Menu from "../themes/default/Menu";
+import RowButton from "../themes/default/RowButton";
+
+export default {
+  title: "MenuBar",
+};
+
+const category = "File";
+const items = [
+  {
+    label: "New",
+    onClick: () => {
+      alert("New");
+    },
+  },
+  {
+    label: "Open",
+    onClick: () => {
+      alert("Open");
+    },
+  },
+  {
+    label: "Save",
+    onClick: () => {
+      alert("Save");
+    },
+  },
+  {
+    label: "Close",
+    onClick: () => {
+      alert("Close");
+    },
+  },
+];
+
+export const withText: FC = () => (
+  <DropdownMenu
+    Button={({ onClick }: { onClick: OnClick }) => (
+      <ToggleButton onClick={onClick}>{category}</ToggleButton>
+    )}
+    List={Menu}
+    options={items.map(({ label, onClick }) => () => (
+      <RowButton selected={false} onClick={onClick}>
+        {label}
+      </RowButton>
+    ))}
+  />
+);
