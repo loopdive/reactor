377ab68410dc5508b2742508d321213b464a58c2
theinfoarchitect <theinfoarchitect@gmail.com>
2020-09-14 13:27:03 +0200

    Remove unused

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
