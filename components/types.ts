6d8e1cd45d657281a9d51e7f7adabd022c583a58
theinfoarchitect <theinfoarchitect@gmail.com>
2020-09-15 10:33:17 +0200

    AnimatedProps type

--- components/types.ts
+++ components/types.ts
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { CSSProperties } from "react";
+import { SpringConfig } from "react-spring";
 
 export type OnClick = (
   event: React.MouseEvent<HTMLButtonElement, MouseEvent>
 ) => void;
+
+export type AnimatedProps = { config?: SpringConfig } & CSSProperties;
