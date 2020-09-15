6e8a53928102ccc2c6aff86c4740279a0b9a1e9e
theinfoarchitect <theinfoarchitect@gmail.com>
2020-09-15 11:52:59 +0200

    - Basic TopBar component and story - Hide on scroll down functionality and flag

---
+++ components/TopBar/index.stories.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+
+import Topbar from "./index";
+
+export default { title: "TopBar" };
+
+export const TopBarTest: FC = () => (
+  <Topbar>
+    <div
+      style={{
+        borderBottom: "1px solid rgba(30, 30, 30, 0.1)",
+        height: 75,
+        backgroundColor: "white",
+      }}
+    >
+      HelloMotto!
+    </div>
+  </Topbar>
+);
---
+++ components/TopBar/index.tsx
@@ -0,0 +1,42 @@
+import React, { FC, ReactNode, useState } from "react";
+
+import { animated, useSpring } from "react-spring";
+
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
+
+type Props = { children: ReactNode; hideOnDownScroll?: boolean };
+
+const TopBar: FC<Props> = ({ children, hideOnDownScroll = false }) => {
+  const [scrollUp, setScrollUp] = useState<boolean>(true);
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      if (hideOnDownScroll) {
+        if (currPos.y > prevPos.y) {
+          setScrollUp(true);
+        } else if (currPos.y < prevPos.y) {
+          setScrollUp(false);
+        }
+      }
+    },
+    [hideOnDownScroll]
+  );
+
+  return (
+    <animated.div
+      style={{
+        ...useSpring({
+          transform: `translate3d(0, ${scrollUp ? "0" : "-100%"}, 0)`,
+        }),
+        position: "sticky",
+        top: "0",
+        left: "0",
+        width: "100%",
+      }}
+    >
+      {children}
+    </animated.div>
+  );
+};
+
+export default TopBar;
