dfb99ac30c44f9e9e2aabde5a84611354c29e8bf
theinfoarchitect <theinfoarchitect@gmail.com>
2020-09-15 13:08:31 +0200

    useScrollFlags

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -6,7 +6,29 @@ import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 type Props = { children: ReactNode; hideOnDownScroll?: boolean };
 
-const TopBar: FC<Props> = ({ children, hideOnDownScroll = false }) => {
+const Topbar: FC<Props> = ({ children, hideOnDownScroll }) => {
+  const { scrollUp, isTop } = useScrollFlags(hideOnDownScroll);
+  return (
+    <animated.div
+      style={{
+        ...useSpring({
+          transform: `translate3d(0, ${scrollUp ? "0" : "-100"}%, 0)`,
+          boxShadow: `0 12px 10px -10px rgba(0, 0, 0, 0${isTop ? "" : ".05"})`,
+        }),
+        position: "sticky",
+        top: "0",
+        left: "0",
+        width: "100%",
+      }}
+    >
+      {children}
+    </animated.div>
+  );
+};
+
+export const useScrollFlags = (
+  hideOnDownScroll: boolean
+): { scrollUp: boolean; isTop: boolean } => {
   const [scrollUp, setScrollUp] = useState<boolean>(true);
   const [isTop, setIsTop] = useState<boolean>(true);
 
@@ -27,22 +49,7 @@ const TopBar: FC<Props> = ({ children, hideOnDownScroll = false }) => {
     [hideOnDownScroll]
   );
 
-  return (
-    <animated.div
-      style={{
-        ...useSpring({
-          transform: `translate3d(0, ${scrollUp ? "0" : "-100"}%, 0)`,
-          boxShadow: `0 12px 10px -10px rgba(0, 0, 0, 0${isTop ? "" : ".05"})`,
-        }),
-        position: "sticky",
-        top: "0",
-        left: "0",
-        width: "100%",
-      }}
-    >
-      {children}
-    </animated.div>
-  );
+  return { scrollUp, isTop };
 };
 
-export default TopBar;
+export default Topbar;
