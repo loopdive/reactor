77d74a0fc667740812484d6f30be96d90cfe972e
Thomas Tränkler <github.com@thomas.traenkler.com>
2020-09-22 15:21:41 +0200

    removed next.js

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-import { FC } from "react";
-import { AppProps } from "next/app";
-
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
