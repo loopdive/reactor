9e4ad3b4cbe9b9a75c2ffe1fa2bdadb28c15a290
theinfoarchitect <theinfoarchitect@gmail.com>
2020-10-02 14:32:43 +0200

    WIP to generalise component

--- src/components/Masonry/index.tsx
+++ src/components/Masonry/index.tsx
@@ -1,16 +1,19 @@
-import React, { FC, /* useEffect, */ useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useMedia } from "./useMedia";
 
-/* import shuffle from "lodash.shuffle"; */
+import shuffle from "lodash.shuffle";
 
 import useMeasure from "react-use-measure";
 import { animated, useTransition } from "react-spring";
 
 import styled from "styled-components";
 
-type Props = { data: { css: string; height: number }[] };
+type Props = {
+  data: { css: string; height: number }[];
+  shuffleItems?: boolean;
+};
 
-const Masonry: FC<Props> = ({ data }) => {
+const Masonry: FC<Props> = ({ data, shuffleItems = false }) => {
   const columns = useMedia(
     [
       "(min-width: 1300px)",
@@ -24,9 +27,22 @@ const Masonry: FC<Props> = ({ data }) => {
 
   const [ref, { width }] = useMeasure();
 
-  const [items /* , setItems */] = useState(data);
+  const [items, setItems] = useState(data);
 
-  /* useEffect(() => void setInterval(() => setItems(shuffle), 10000), []); */
+  useEffect(() => {
+    let interval: number;
+    if (shuffleItems) {
+      interval = setInterval(() => {
+        setItems(shuffle);
+      }, 10000);
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, []);
 
   const heights = new Array(columns).fill(0);
 
@@ -50,57 +66,50 @@ const Masonry: FC<Props> = ({ data }) => {
 
   return (
     <List ref={ref} style={{ height: Math.max(...heights) }}>
-      {
+      {transitions.map(
         // @ts-ignore
-        transitions.map(({ item, props: { xy, ...rest }, key }) => {
-          console.log(xy);
-          return (
-            <animated.div
-              key={key}
-              style={{
-                transform: xy.interpolate(
-                  (x: number, y: number) => `translate3d(${x}px,${y}px,0)`
-                ),
-                ...rest,
-              }}
-            >
-              <div style={{ backgroundImage: item.css }} />
-            </animated.div>
-          );
-        })
-      }
+        ({ item: { css: backgroundImage }, props: { xy, ...rest }, key }) => (
+          // @ts-ignore
+          <ItemContainer
+            key={key}
+            style={{
+              transform: xy.interpolate(
+                (x: number, y: number) => `translate3d(${x}px,${y}px,0)`
+              ),
+              ...rest,
+            }}
+          >
+            <Item style={{ backgroundImage }} />
+          </ItemContainer>
+        )
+      )}
     </List>
   );
 };
 
 export const List = styled.div`
   position: relative;
-  font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir,
-    helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif;
   width: 100%;
   height: 100%;
+`;
 
-  & > div {
-    position: absolute;
-    will-change: transform, width, height, opacity;
-    padding: 15px;
-  }
+const ItemContainer = styled(animated.div)`
+  position: absolute;
+  will-change: transform, width, height, opacity;
+  padding: 15px;
+`;
 
-  & > div > div {
-    position: relative;
-    background-size: cover;
-    background-color: white;
-    background-position: center center;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    text-transform: uppercase;
-    font-size: 10px;
-    line-height: 10px;
-    border-radius: 4px;
-    box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 0.2);
-    margin: 10px;
-  }
+const Item = styled.div`
+  position: relative;
+  background-size: cover;
+  background-color: white;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 4px;
+  box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 0.2);
+  margin: 10px;
 `;
 
 export default Masonry;
