eb86e5b482289129aa7dbefad777b8f6cec22599
theinfoarchitect <theinfoarchitect@gmail.com>
2020-10-17 15:36:00 +0200

    Map FC components to infinite carousel

--- src/components/InfiniteCarousel/index.stories.tsx
+++ src/components/InfiniteCarousel/index.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, forwardRef } from "react";
 import InfiniteCarouselV1 from ".";
 import InfiniteCarouselV2 from "./v2";
 
@@ -37,6 +37,22 @@ export const TestInfiniteCarouselV1: FC = () => {
   );
 };
 
+const images = [
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
+];
+
+const Image: FC<{ src: string }> = forwardRef<
+  HTMLImageElement,
+  { src: string }
+>(({ src }, ref) => {
+  return <img src={src} alt="" style={{ height: 100, width: 250 }} ref={ref} />;
+});
 export const TestInfiniteCarouselV2: FC = () => {
   return (
     <div
@@ -47,48 +63,9 @@ export const TestInfiniteCarouselV2: FC = () => {
       }}
     >
       <InfiniteCarouselV2 speed={0.25}>
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
-          height="100"
-          width="250"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
-          height="100"
-          width="250"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
-          height="100"
-          width="250"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
-          height="100"
-          width="250"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
-          height="100"
-          width="250"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
-          height="100"
-          width="250"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
-          height="100"
-          width="250"
-          alt=""
-        />
+        {images.map((src) => (
+          <Image key={src} src={src} />
+        ))}
       </InfiniteCarouselV2>
     </div>
   );
