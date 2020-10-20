664023f03172f379698635f85277b50eda0b9a14
theinfoarchitect <theinfoarchitect@gmail.com>
2020-10-20 13:56:13 +0200

    Refactor to get DropdownMenu working

--- src/components/DropdownMenu/index.stories.tsx
+++ src/components/DropdownMenu/index.stories.tsx
@@ -1,33 +1,31 @@
 import * as React from "react";
 import { FC } from "react";
-import { ThemeProvider } from "styled-components";
 import DropdownMenu from ".";
 import { OnClick } from "../types";
-import { useThemeSelector } from "../../utils/hooks/useThemeSelector";
+
+import "./style.module.css";
+import { jsx } from "../../utils";
 
 export default {
   title: "DropdownMenu",
 };
 
 export const WithText: FC = () => {
-  const [ThemeSelector, theme] = useThemeSelector(dark, { dark, light });
-
   return (
     <>
-      <ThemeSelector />
-      <ThemeProvider theme={theme}>
-        <DropdownMenu
-          Button={({ onClick }: { onClick: OnClick }) => (
-            <ToggleButton onClick={onClick}>{category}</ToggleButton>
-          )}
-          List={Menu}
-          options={items.map(({ label, onClick }) => () => (
-            <RowButton selected={false} onClick={onClick}>
-              {label}
-            </RowButton>
-          ))}
-        />
-      </ThemeProvider>
+      <DropdownMenu
+        Button={({ onClick }: { onClick: OnClick }) => (
+          <button className="toggle-button" onClick={onClick}>
+            {category}
+          </button>
+        )}
+        List={jsx(<div className="menu" />)}
+        options={items.map(({ label, onClick }) => () => (
+          <button className="row-button" onClick={onClick}>
+            {label}
+          </button>
+        ))}
+      />
     </>
   );
 };
